fdeba10fba343f4f48f716766e62a851c4815ace
Yoyo99 <lauboin@free.fr>
2025-05-02 19:20:00 +0200

    Update

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,4 +1,5 @@
 // Formate un nombre en prix français (ex: 1 234,56 €) pour n'avoir que deux décimales après la virgule.
+// Version corrigée.
 export function formatPrice(value: number): string {
   return new Intl.NumberFormat('fr-FR', {
     style: 'currency',
