75797534ac9426ee720766d92de5a3793da4d2c3
Yoyo99 <lauboin@free.fr>
2025-05-31 20:56:00 +0200

    Update DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -159,7 +159,7 @@ export function DashboardLayout({ children }: { children?: React.ReactNode }) {
         </Dialog>
       </Transition.Root>
 
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-60 lg:flex lg:w-72 lg:flex-col">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-background px-6 pb-4 border-r border-white/10">
           <div className="flex h-16 shrink-0 items-center">
             <Link to="/" className="text-2xl font-bold bg-gradient-to-r from-primary-400 to-secondary-400 text-transparent bg-clip-text">
