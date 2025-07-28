9f26b87a3194b62b096e329fbde3aac4b3961637
Yoyo99 <lauboin@free.fr>
2025-07-28 17:57:22 +0200

    Update et Fix parse-cv

--- supabase/functions/parse-cv/index.ts
+++ supabase/functions/parse-cv/index.ts
@@ -12,7 +12,7 @@ const supabase = createClient(
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-supabase-edge-version',
 };
 
 Deno.serve(async (req) => {
